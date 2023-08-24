6dfa98e441bd9f93d4c90804e2bb152464d82ad7
Josh Gillum <jmgillum@nic.edu>
2023-08-23 19:49:05 -0700

    Created career structure

--- analytics.h
+++ analytics.h
@@ -1,1 +1,48 @@
+#ifndef ANALYTICS_H
+#define ANALYTICS_H
 
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+
+struct career{
+    career() : career("",0,0,0,0,0,0,0,0,0,0,0) {
+        
+    }
+    career(std::string fieldName, double meanSal, double medSal,
+     unsigned pop, unsigned asian, unsigned minority, 
+     unsigned white, unsigned female, unsigned male, unsigned degreeB,
+     unsigned degreeM, unsigned degreeD){
+        name = fieldName;
+        meanSalary = meanSal;
+        medianSalary = medSal;
+        totalPopulation = pop;
+        numAsians = asian;
+        numMinorities = minority;
+        numWhites = white;
+        numFemales = female;
+        numMales = male;
+        numBachelors = degreeB;
+        numMasters = degreeM;
+        numDoctorate = degreeD;
+    }
+
+
+    unsigned totalPopulation,
+        numAsians,
+        numMinorities,
+        numWhites,
+        numFemales,
+        numMales,
+        numBachelors,
+        numDoctorate,
+        numMasters;
+    std::string name;
+    double meanSalary,
+        medianSalary;
+};
+
+
+#endif
